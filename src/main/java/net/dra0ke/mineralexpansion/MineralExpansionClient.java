a675656d191a8d56121c3b3f6c7118b3dc47ac0d
Dra0ke <aalek854@gmail.com>
2024-11-09 00:25:12 +0100

    all files

---
+++ src/main/java/net/dra0ke/mineralexpansion/MineralExpansionClient.java
@@ -0,0 +1,10 @@
+package net.dra0ke.mineralexpansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineralExpansionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
