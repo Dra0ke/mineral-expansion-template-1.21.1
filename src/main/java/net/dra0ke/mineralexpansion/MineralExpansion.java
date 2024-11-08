0abe608810a820f146e13ecc4b6351f9c314b5ce
Dra0ke <aalek854@gmail.com>
2024-11-09 00:24:19 +0100

    yes

---
+++ src/main/java/net/dra0ke/mineralexpansion/MineralExpansion.java
@@ -0,0 +1,15 @@
+package net.dra0ke.mineralexpansion;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// yes
+public class MineralExpansion implements ModInitializer {
+	public static final String MOD_ID = "mineral-expansion";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
